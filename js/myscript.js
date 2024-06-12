379448a18e3f480c8c285c686fed6749ae366bca
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-12 15:13:36 +0200

    Esercizio Parola palindroma completato

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,21 @@
+// parola palindoma
 
+function parolapalindroma(parola) {
+    let caratteriparola = parola.split("");
+    let parolainvertita = caratteriparola.reverse("").join("");
+    return parolainvertita
+}
 
+let parolainiziale = prompt("inserisci una parola");
 
+const parolarisultato = parolapalindroma(parolainiziale);
 
+if (parolarisultato == parolainiziale) {
+    document.querySelector(".risultato").innerHTML = "La parola " + parolainiziale + " è palindroma";
+} else {
+    document.querySelector(".risultato").innerHTML = "La parola " + parolainiziale + " non è palindroma";
+}
 
-
-
-
-
-
-
+// pari e dispari
 
 
