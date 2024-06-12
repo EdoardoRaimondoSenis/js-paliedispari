2f2202f0b0b8fbb43e347682c24139451cc2ad35
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-12 16:22:27 +0200

    Esercizio pari o dispari completato

--- js/myscript.js
+++ js/myscript.js
@@ -11,11 +11,53 @@ let parolainiziale = prompt("inserisci una parola");
 const parolarisultato = parolapalindroma(parolainiziale);
 
 if (parolarisultato == parolainiziale) {
-    document.querySelector(".risultato").innerHTML = "La parola " + parolainiziale + " è palindroma";
+    document.querySelector(".risultatopalind").innerHTML = "La parola " + parolainiziale + " è palindroma";
 } else {
-    document.querySelector(".risultato").innerHTML = "La parola " + parolainiziale + " non è palindroma";
+    document.querySelector(".risultatopalind").innerHTML = "La parola " + parolainiziale + " non è palindroma";
 }
 
 // pari e dispari
 
+let paridispari = prompt("Scegli: Pari o Dispari?")
+
+if (paridispari == "Pari" || paridispari == "Dispari") {
+    let numerounocinque = parseInt(prompt("Inserisci un numero da 1 a 5"));
+    if (numerounocinque > 0 && numerounocinque <= 5) {
+        function numerocpu(max, min) {
+            return Math.floor(Math.random() * (max - min) + min);
+        }
+        
+        console.log(numerounocinque)
+        let numerocaso = numerocpu(1, 5);
+        console.log(numerocaso)
+
+        function sommatotale(num1, num2) {
+            let somma = num1+ num2;
+            return somma;
+        }
+
+        let somma = sommatotale(numerounocinque, numerocaso);
+        console.log(somma)
+
+        if (paridispari == "Pari") {
+            if (somma % 2 == 0) {
+                document.querySelector(".risultatopd").innerHTML = "La somma dei due numeri è " + somma + " ed è pari, Complimenti Hai vinto!"
+            } else if (somma % 3 == 0) {
+                document.querySelector(".risultatopd").innerHTML = "La somma dei due numeri è " + somma + " ed è dispari, Ha vinto la CPU!"
+            }
+        } else if (paridispari == "Dispari") {
+            if (somma % 3 == 0) {
+                document.querySelector(".risultatopd").innerHTML = "La somma dei due numeri è " + somma + " ed è dispari, Complimenti Hai vinto!"
+            } else if (somma % 2 == 0) {
+                document.querySelector(".risultatopd").innerHTML = "La somma dei due numeri è " + somma + " ed è pari, Ha vinto la CPU"
+            }
+        }
+    
+    } else {
+        alert("Il numero inserito o è troppo grande");
+        
+    }
+} else {
+    alert("Scrivi Pari o Dispari o non giochiamo eh");
+}
 
